33f7fcdc8188483c530267c6adec2aa520601f34
suhaniswami <69630717+suhaniswami@users.noreply.github.com>
2020-08-18 18:18:33 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,41 +1,69 @@
-var movingrectangle
-var fixedrectangle
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
 
 function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-  createCanvas(800,400);
-  movingrectangle=createSprite(600,200,10,10);
-  fixedrectangle=createSprite(200,200,20,20);
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
 
-movingrectangle.shapeColor="green";
-fixedrectangle.shapeColor="green";
-movingrectangle.debug=true;
-fixedrectangle.debug=true;
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
 
 
+	engine = Engine.create();
+	world = engine.world;
+
+   
+   packageBody = Bodies.circle(width/2 , 200 , 5 , 20);
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
 }
+
 
 function draw() {
-  background(0,0,0);  
-movingrectangle.x=World.mouseX;
-movingrectangle.y=World.mouseY;
-console.log(movingrectangle.x-fixedrectangle.x)
-console.log(movingrectangle.width/2+fixedrectangle.width/2)
-if(movingrectangle.x-fixedrectangle.x<movingrectangle.width/2+fixedrectangle.width/2 &&
-fixedrectangle.x-movingrectangle.x<movingrectangle.width/2+fixedrectangle.width/2 &&
- movingrectangle.y-fixedrectangle.y<movingrectangle.height/2+fixedrectangle.height/2 
-&&fixedrectangle.y-movingrectangle.y<movingrectangle.height/2+fixedrectangle.height/2){
-  movingrectangle.shapeColor="red";
-  fixedrectangle.shapeColor="red";
-
-}
-else{
-  movingrectangle.shapeColor="green";
-
-fixedrectangle.shapeColor="green";
-}
-
-
-
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  
   drawSprites();
+
+ 
 }
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+   Matter.body.SetStatic(packageBody,false);
+  }
+  
+}
+
+
+
