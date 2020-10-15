a636ec7dd2a729a9859af7548a410adbf2bd06fb
suhani swami <69630717+suhaniswami@users.noreply.github.com>
2020-10-15 14:45:50 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,100 +1,49 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
-var rect1,rect2,rect3
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-
-function preload()
-{
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
-
-	
-		
-
-
-	
-}
+var ground , ground1 ,ground2 
+var car,car1,car2,car3
+var go,go1,go2,go3
 
 function setup() {
-	createCanvas(800, 700);
-	rectMode(CENTER);
-	var packageBody_options = {
-		isStatic: true
-	     
-	}
-	var packageBody_options = {
-		restitution:0
+  createCanvas(800,400);
+  ground=createSprite(200,100,1200,10);
+  ground1=createSprite(200,200,1200,10);
+  ground2=createSprite(200,300,1200,10);
 
-	}
+   car=createSprite(750,50,50,20);
+   car1=createSprite(750,150,50,20);
+   car2=createSprite(750,250,50,20);
+   car3=createSprite(750,350,50,20);
 
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+ go=createSprite(50,50,20,20)
+ go.velocityX=4;
+ go1=createSprite(50,150,20,20)
+ go1.velocityX=4;
+ go2=createSprite(50,250,20,20)
+ go2.velocityX=4;
+ go3=createSprite(50,350,20,20)
+ go3.velocityX=4;
 
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
-
-rect1=createSprite(400,650,250,10);
-rect2=createSprite(270,610,10,90);
-rect3=createSprite(530,610,10,90)
-
-
-
-
-	engine = Engine.create();
-	world = engine.world;
-
-
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic: true});
-	World.add(world, packageBody);
-	
-
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 630, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-
-
-	Engine.run(engine);
-  
 }
-
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
-  rect1.shapeColor="red";
-  rect2.shapeColor="red";
-  rect3.shapeColor="red";
+  background(0); 
+  if( car.x - go.x < go.width/2 + car.width/2){
+    go.shapeColor = "Red";
+  }
+
+  if( car1.x - go1.x < go1.width/2 + car1.width/2){
+    go1.shapeColor = "Red";
+  }
+ 
+  if( car2.x - go2.x < go2.width/2 + car2.width/2){
+    go2.shapeColor = "Red";
+  }
+
+
+  if( car3.x - go3.x < go3.width/2 + car3.width/2){
+    go3.shapeColor = "Red";
+  }
+
+
 
   drawSprites();
- 
 }
-
-function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on
-	Matter.Body.setStatic(packageBody,false);
-	
-
-	
-  }
- 
-  }
-
-
-
-
-
-
-
-
-
